ab8430adc93f0451950c6380ceb02b1babeebed7
Asher Norland <asher.norland@gmail.com>
2025-04-02 19:12:15 -0400

    Comments

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -3,6 +3,7 @@ import { createResolver } from '@nuxt/kit';
 const { resolve } = createResolver(import.meta.url);
 
 export default defineNuxtConfig({
+	// Reference the parent directory's module
 	modules: ['../src/module'],
 	devtools: {
 		enabled: true,
@@ -15,6 +16,7 @@ export default defineNuxtConfig({
 	},
 	compatibilityDate: '2024-11-12',
 
+	// The stylesheet does not exist in this workspace, so we have to reference the parent's stubbed distribution
 	vite: {
 		resolve: {
 			alias: {
